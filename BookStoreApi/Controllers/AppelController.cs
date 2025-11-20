dda756c34d33d30110261e7758165933b6ef4e46
AMWassim <wassim2001.11.1@gmail.com>
2025-11-20 15:37:26 +0100

    ahhhhhh

---
+++ BookStoreApi/Controllers/AppelController.cs
@@ -0,0 +1,25 @@
+﻿using BookStoreApi.Models;
+using BookStoreApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppelController : ControllerBase
+    {
+        public AppelController() { }
+
+        // GET: api/<PizzaController>
+        [HttpGet]
+        public IActionResult GetHello() {
+            return Ok(3);
+        }
+
+
+
+    }
+}
--- BookStoreApi/Program.cs
+++ BookStoreApi/Program.cs
@@ -21,10 +21,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200") // ton app Angular
+            policy.AllowAnyOrigin() // ton app Angular
                   .AllowAnyHeader()
-                  .AllowAnyMethod()
-                  .AllowCredentials();
+                  .AllowAnyMethod();
         });
 });
 
